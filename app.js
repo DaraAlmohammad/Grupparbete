de8b28c44792927c863b5a8471f4f7f78c362c06
Oggylito <thordeneddie@gmail.com>
2026-05-05 15:48:06 +0200

    Menu: alternative wording for menu.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function main() {
-    console.log("Calculator v0.1"); 
+    console.log("Calculator v0.1");
+    console.log("1) add \n0) quit");
+    let choice = prompt("Select option: ");
 }
 
 main();
