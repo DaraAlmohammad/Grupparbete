3df4daa79514169aa3125798dc4ea81ec639f15e
00Darios9 <daraalmohammad@gmail.com>
2026-05-05 15:48:57 +0200

    add basic menu output

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function main() {
-    console.log("Calculator v0.1"); 
+    console.log("Calculator v0.1");
+    console.log("1) Add\n0) Exit");
+    let choice = prompt("choice: "); 
 }
 
 main();
