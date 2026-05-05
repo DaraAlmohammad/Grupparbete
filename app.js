cf546bf5db7798d545e6005c5642b1d2cefcca4e
Oggylito <thordeneddie@gmail.com>
2026-05-05 16:00:32 +0200

    Read first operand.

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ function main() {
     console.log("Calculator v0.1"); 
     console.log("1) Addition\n0) Exit program");
     let choice = prompt("Your choice: ");
-
 }
+
+let a = parseFloat(prompt("a: "));
 
 main();
 
