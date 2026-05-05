7d86e1d33ef2f2646a8f43d658b2840468afcd9d
RuthGranath <ruth.granath@yahoo.com>
2026-05-05 16:06:25 +0200

    Call add and print result

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ import{add} from "./calculator.js";
 function main() {
     console.log("Calculator v0.1"); 
     console.log("1) Addition\n0) Exit program");
+
+    let a = parseFloat(prompt("a: "));
+    let b = parseFloat(prompt("b: "));
     let choice = prompt("Your choice: ");
+    
+    if (choice === "1") {
+        console.log(add(a, b));
+    }
 }
 
-let a = parseFloat(prompt("a: "));
-let b = parseFloat(prompt("b: "));
 main();
 
